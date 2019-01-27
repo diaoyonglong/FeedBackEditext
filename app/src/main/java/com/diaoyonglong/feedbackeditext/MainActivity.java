d0cc50472576310f0c0512fc0b08f4629e41745d
diaoyonglong <diaoyonglong@aoji.cn>
2019-01-27 22:21:38 +0800

    创建分支1.0.0

--- app/src/main/java/com/diaoyonglong/feedbackeditext/MainActivity.java
+++ app/src/main/java/com/diaoyonglong/feedbackeditext/MainActivity.java
@@ -3,11 +3,23 @@ package com.diaoyonglong.feedbackeditext;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.diaoyonglong.editext.FeedBackEditext;
+
+/**
+ * Created by diaoyonglong on 2019/1/27
+ *
+ * @desc FeedBackEditext  用法
+ */
+
 public class MainActivity extends AppCompatActivity {
+
+    FeedBackEditext editext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editext = (FeedBackEditext) findViewById(R.id.editext);
     }
 }
