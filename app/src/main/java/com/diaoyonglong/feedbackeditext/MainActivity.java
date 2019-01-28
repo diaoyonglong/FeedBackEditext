0658b33a41d1e8f280f9677a90a9cda72f6b76a4
diaoyonglong <diaoyonglong@aoji.cn>
2019-01-28 14:43:45 +0800

    添加注释

--- app/src/main/java/com/diaoyonglong/feedbackeditext/MainActivity.java
+++ app/src/main/java/com/diaoyonglong/feedbackeditext/MainActivity.java
@@ -1,7 +1,10 @@
 package com.diaoyonglong.feedbackeditext;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.diaoyonglong.editext.FeedBackEditext;
 
@@ -14,6 +17,7 @@ import com.diaoyonglong.editext.FeedBackEditext;
 public class MainActivity extends AppCompatActivity {
 
     FeedBackEditext editext;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +25,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         editext = (FeedBackEditext) findViewById(R.id.editext);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, editext.getContent(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
